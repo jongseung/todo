dcde46dbf92e74cf2e339469742ecea65e8a55fa
jongseung <park930902@gmail.com>
2018-12-10 19:20:13 +0900

    TodoList Delete 버튼 구현
    TodoList Delete 버튼 구현
    
    filiter 함수를 이용해서 localstoage에 삭제 후 저장

--- todo.js
+++ todo.js
@@ -3,16 +3,30 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "todos";
-const toDos = [];
+let toDos = [];
+
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
+
+function deleteTodo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
 function printToDo(text) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   const delBtn = document.createElement("button");
   const newId = toDos.length + 1;
   delBtn.innerText = "✔";
+  delBtn.addEventListener("click", deleteTodo);
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
